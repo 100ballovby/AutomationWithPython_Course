a1281df64ff720d7a59f68c28b35456e0b4ad3a4
demidraksin <greatraksin@icloud.com>
2021-07-26 12:59:14 +0300

    Adding fileIO lesson🖖

---
+++ 02_lesson_2607/00_turtle.py
@@ -0,0 +1,12 @@
+from turtle import *
+
+
+t = Turtle()
+t.color('#0046b8')
+#  #RRGGBB
+
+for i in range(10):
+    t.fd(40)
+    t.lt(360 / 10)
+
+done()
---
+++ 02_lesson_2607/filesIO.py
@@ -0,0 +1,7 @@
+# Объект, у которого есть 2 важных элемента - имя и расширение
+import os  # позволяет работать с файлами операционной системы
+
+# Windows: C:\Data\Users\folder\file.txt
+# Linux/Mac: /Data/Users/folder/file.txt
+path = os.path.join('Documents', 'News', 'file.txt')
+
