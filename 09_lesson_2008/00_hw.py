6310769bea45f3259fca76fa7813bcaee7c63616
demidraksin <greatraksin@icloud.com>
2021-08-20 11:24:55 +0300

    Finishing homework

--- 09_lesson_2008/00_hw.py
+++ 09_lesson_2008/00_hw.py
@@ -1,6 +1,25 @@
 import requests
 import json
 
-url = 'https://api.github.com/users/100ballovby/repos'
-res = requests.get(url)
-print(res.text)
+
+def get_repos_from_github(username):
+    """
+    Функция подключается к гитхабу, находит полоьзователя и
+    собирает все его репозитории в словарь.
+    :param username: имя пользователя на github.com
+    :return: возвращает словарь, где ключ - название репозитория, а значение - ссылка на него.
+    """
+    url = f'https://api.github.com/users/{username}/repos'
+    res = requests.get(url)
+    json_res = json.loads(res.text)
+
+    repositories = {}
+
+    for repo in json_res:
+        repositories[repo['name']] = repo['svn_url']
+
+    return repositories
+
+sto_ballov = get_repos_from_github('100ballovby')
+print(sto_ballov)
+
